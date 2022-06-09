859b96c296dc71bccd5a011cd5183193f9a8cf55
nnakki <yvely225@gmail.com>
2022-06-09 14:49:58 +0900

    Initial commit

---
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package com.jojoldu.book.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ComponentScan(basePackages={"com.jojoldu.book.springboot.web"})
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void hello가리턴() throws Exception{
+        String hello = "hello";
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
